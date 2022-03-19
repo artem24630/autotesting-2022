5bb50676061355f149a3f91ea79227cefc3ba045
Artyom Trofimov <a.t.0202@bk.ru>
2022-03-19 18:55:59 +0300

    add some tests

--- src/test/java/tests/MainPageTest.java
+++
@@ -1,5 +0,0 @@
-package tests;
-
-public class MainPageTest extends AbstractTest {
-
-}
--- src/test/java/tests/MessagePageTest.java
+++ src/test/java/tests/MessagePageTest.java
@@ -1,10 +1,13 @@
 package tests;
 
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.image;
 import pages.LoginPage;
 import pages.MessagePage;
 import pages.elements.ChatsPageElement;
 import utils.UserDetails;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -20,7 +23,11 @@ class MessagePageTest extends AbstractTest {
         messagePage.get();
         ChatsPageElement chats = messagePage.getChats();
         if (!chats.getChats().isEmpty()) {
-            chats.getChats().first().find(By.xpath("//img")).shouldHave(image);
+            SelenideElement element = chats.getChats().first();
+            element.find(By.xpath(".//a[contains(@href, '/messages/')]")).should(exist);
+            element.find(By.xpath(".//img")).shouldHave(image);
+            element.find(By.xpath(".//div[@data-tsid='conversation_name']//msg-parsed-text")).shouldNotBe(empty);
+            element.find(By.xpath(".//div[@data-tsid='last_message']//msg-parsed-text")).shouldNotBe(empty);
         }
     }
 }
