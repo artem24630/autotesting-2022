baa0ced98532e0b3b7d3acdd86878db33319e1d0
Artyom Trofimov <a.t.0202@bk.ru>
2022-03-19 16:48:23 +0300

    init

---
+++ src/test/java/AbstractTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public abstract class AbstractTest {
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(driver);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        WebDriverRunner.closeWebDriver();
+        driver.close();
+    }
+}
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.support.ui.LoadableComponent;
+
+public class LoginPage extends LoadableComponent<LoginPage> {
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+
+    }
+}
