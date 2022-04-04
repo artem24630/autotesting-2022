458004af34febefddfa7b711c3d81043905cce7b
PaaavelZ <paaavel.2024@gmail.com>
2022-04-04 10:00:10 +0300

    Make some cosmetic changes

--- src/test/java/pages/NewsPage.java
+++ src/test/java/pages/NewsPage.java
@@ -6,13 +6,11 @@ import org.openqa.selenium.support.ui.LoadableComponent;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class NewsPage extends LoadableComponent<NewsPage> {
 
     private static final By NEWS_FEED = By.id("hook_Block_UserMainFullMRB");
     private static final By NEWS_BUTTON = By.xpath("//li[@data-l='t,userMain']");
-    private static final By MESSAGES_BUTTON = By.xpath("//li[@data-l='t,messages']");
 
     @Override
     protected void load() {
@@ -22,11 +20,6 @@ public class NewsPage extends LoadableComponent<NewsPage> {
     @Override
     protected void isLoaded() throws Error {
         $(NEWS_FEED).should(exist).shouldBe(visible);
-    }
-
-    public MessagePage clickOnMessages() {
-        $(MESSAGES_BUTTON).shouldBe(visible.because("Messages button isn't displayed")).click();
-        return new MessagePage();
     }
 
 }
--- src/test/java/tests/MessagePageTest.java
+++ src/test/java/tests/MessagePageTest.java
@@ -1,15 +1,26 @@
 package tests;
 
+import org.openqa.selenium.By;
 import pages.MessagePage;
 import org.junit.jupiter.api.Test;
 import pages.NewsPage;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 class MessagePageTest extends AbstractTest {
+
+    private static final By MESSAGES_BUTTON = By.xpath("//li[@data-l='t,messages']");
 
     @Test
     void correctMessagePage() {
-        MessagePage messagePage = new NewsPage().clickOnMessages();
+        MessagePage messagePage = clickOnMessages();
         messagePage.get();
+    }
+
+    private MessagePage clickOnMessages() {
+        $(MESSAGES_BUTTON).shouldBe(visible.because("Messages button isn't displayed")).click();
+        return new MessagePage();
     }
 
 }
